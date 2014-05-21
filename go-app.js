3ca02ca636f1853c049adcf75bbddf4581f63247
gsvr <gsvanrooyen@gmail.com>
2014-05-21 13:59:57 +0200

    update flow to latest sow

--- go-app.js
+++ go-app.js
@@ -24,25 +24,8 @@ go.app = function() {
 
                 next: function(choice) {
                     return {
-                        yes: 'states:age',
-                        no: 'states:end_no_consent'
-                    } [choice.value];
-                }
-            });
-        });
-
-        self.states.add('states:age', function(name) {
-            return new ChoiceState(name, {
-                question: $('Are you older than 18 years? Text 1 for Yes and 2 for No'),
-
-                choices: [
-                    new Choice('yes', 'Yes'),
-                    new Choice('no', 'No')],
-
-                next: function(choice) {
-                    return {
                         yes: 'states:language',
-                        no: 'states:end_under_age'
+                        no: 'states:end_no_consent'
                     } [choice.value];
                 }
             });
@@ -56,6 +39,20 @@ go.app = function() {
                     new Choice('english', 'English'),
                     new Choice('amharic', 'Amharic'),
                     new Choice('ኣማርኛ', 'ኣማርኛ')],
+
+                next: function(choice) {
+                    return 'states:age';
+                }
+            });
+        });
+
+        self.states.add('states:age', function(name) {
+            return new ChoiceState(name, {
+                question: $('Are you older than 18 years? Text 1 for Yes and 2 for No'),
+
+                choices: [
+                    new Choice('yes', 'Yes'),
+                    new Choice('no', 'No')],
 
                 next: function(choice) {
                     return 'states:gender';
@@ -82,21 +79,6 @@ go.app = function() {
                 question: $('Please enter your Listener Group Name'),
 
                 next: function(content) {
-                    return 'states:group_type';
-                }
-            });
-        });
-
-        self.states.add('states:group_type', function(name) {
-            return new ChoiceState(name, {
-                question: $('What is your group type? Text 1 for Mixed group, 2 or Girls only and 3 for Gatekeepers'),
-
-                choices: [
-                    new Choice('mixed', 'Mixed'),
-                    new Choice('girls_only', 'Girls Only'),
-                    new Choice('gatekeepers', 'Gatekeepers')],
-
-                next: function(choice) {
                     return 'states:end_registered';
                 }
             });
@@ -105,23 +87,15 @@ go.app = function() {
         // End State 1 - no consent
         self.states.add('states:end_no_consent', function(name) {
             return new EndState(name, {
-                text: $('Thank you, you will not receive any survey questions.'),
+                text: $('Thank you for reaching out to Yegna Listener Group Registration Survey'),
                 next: 'states:start'
             });
         });
 
-        // End State 2 - age not 18
-        self.states.add('states:end_under_age', function(name) {
-            return new EndState(name, {
-                text: $('Thank you. Unfortunately, you are too young to partake in the surveys.'),
-                next: 'states:start'
-            });
-        });
-
-        // End State 3 - successful registration
+        // End State 2 - successful registration
         self.states.add('states:end_registered', function(name) {
             return new EndState(name, {
-                text: $('Thank you. You are now registered to receive survey questions.'),
+                text: $('Thank you! You are now registered as a member of the Yegna Listener Group. Please share your feedback with us every week. Your input is very important to us.'),
                 next: 'states:start'
             });
         });
